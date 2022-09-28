a25cf88c76f5adbaaab4a292829cda5f80f8591a
abhishek <abhiskg20@gmail.com>
2022-09-28 22:02:08 +0600

    Activity data loaded using useEffect and stored in state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
 import { Activity } from "../components/Activity/Activity";
 import { Details } from "../components/Details/Details";
 
 export const Home = () => {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    const loadActivities = async () => {
+      const res = await fetch("data/data.json");
+      const data = await res.json();
+      setActivities(data);
+    };
+    loadActivities();
+  }, []);
+
   return (
     <div className="bg-gray-100">
       <div className="w-9/12 mx-auto">
