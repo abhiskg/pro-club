923181516e6d3e11f22f730ff99f75cf8d19e28c
abhishek <abhiskg20@gmail.com>
2022-09-29 00:01:23 +0600

    Exercise Details layout created

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,3 +1,37 @@
 export const Details = () => {
-  return <div></div>;
+  return (
+    <div className="px-4 mt-5 sticky top-5">
+      <img
+        className="w-12 h-12 rounded-full mx-auto"
+        src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+        alt=""
+      />
+      <h2 className="font-semibold text-center">Abhishek Sengupta</h2>
+      <p className="text-gray-500 text-sm text-center">Chittagong Bangladesh</p>
+      <div className="flex gap-5 mt-4 items-center justify-center ">
+        <div>
+          <div className="font-medium text-center">75kg</div>
+          <div className="text-sm text-gray-500">Weight</div>
+        </div>
+        <div>
+          <div className="font-medium text-center">6.5</div>
+          <div className="text-sm text-gray-500 text-center">Height</div>
+        </div>
+        <div>
+          <div className="font-medium text-center">25yrs</div>
+          <div className="text-sm text-gray-500 text-center">Age</div>
+        </div>
+      </div>
+      <h2 className="mt-5 font-medium">Add a Break</h2>
+      <h2 className="mt-5 font-medium">Exercise Details</h2>
+      <div>Exercise Time:</div>
+      <div>Break Time:</div>
+      <button className="w-full bg-orange-200 rounded py-1.5 mt-3">
+        Clear Details
+      </button>
+      <button className="w-full bg-orange-200 rounded py-1.5 mt-3">
+        Activity Completed
+      </button>
+    </div>
+  );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,7 +15,7 @@ export const Home = () => {
   }, []);
 
   return (
-    <div className="bg-gray-100">
+    <div className="bg-gray-100 flex relative">
       <div className="w-9/12 mx-auto">
         <h1>PRO-CLUB</h1>
         <h2>Select today's activity</h2>
@@ -25,7 +25,7 @@ export const Home = () => {
           ))}
         </div>
       </div>
-      <div className="sticky top-0 bg-white">
+      <div className="relative bg-white">
         <Details />
       </div>
     </div>
